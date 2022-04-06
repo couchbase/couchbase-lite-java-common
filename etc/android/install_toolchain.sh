d11fa7f5cfa61d73467247bb94dc5f21703daddf
G. Blake Meike <blake.meike@couchbase.com>
2022-04-06 15:46:18 -0700

    Move script to etc dir

--- etc/android/install_toolchain.sh
+++ etc/android/install_toolchain.sh
@@ -2,7 +2,8 @@
 #
 # Install the Android toolchain
 # Don't execute this file, source it
-#
+# This script expects to inherit the variable BIN_DIR,
+# the name of a directory into which it can install tools
 
 # These versions must match the versions in lib/build.gradle
 NINJA_VERSION="1.10.2"
@@ -13,11 +14,11 @@ NDK_VERSION='23.1.7779620'
 echo "======== Install Toolchain"
 
 cbdep install -d "${BIN_DIR}" ninja ${NINJA_VERSION}
-NINJA_DIR="${BIN_DIR}"/ninja-*
+NINJA_DIR=`echo "${BIN_DIR}"/ninja-*`
 export PATH="${NINJA_DIR}/bin:${PATH}"
 
 cbdep install -d "${BIN_DIR}" cmake ${CMAKE_VERSION}
-CMAKE_DIR="${BIN_DIR}"/cmake-*
+CMAKE_DIR=`echo "${BIN_DIR}"/cmake-*`
 export PATH="${CMAKE_DIR}/bin:${PATH}"
 
 yes | ${SDK_MGR} --licenses > /dev/null 2>&1
