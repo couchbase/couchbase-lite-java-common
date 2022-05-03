f9a61e6c249b64b944a23dac304151090fdf8d58
G. Blake Meike <blake.meike@couchbase.com>
2022-05-03 09:21:29 -0700

    Use try-with-resources in unzip util

--- common/test/java/com/couchbase/lite/internal/utils/ZipUtils.java
+++ common/test/java/com/couchbase/lite/internal/utils/ZipUtils.java
@@ -26,27 +26,25 @@ import java.util.zip.ZipInputStream;
 public class ZipUtils {
     public static void unzip(InputStream in, File destination) throws IOException {
         byte[] buffer = new byte[1024];
-        ZipInputStream zis = new ZipInputStream(in);
-        ZipEntry ze = zis.getNextEntry();
-        while (ze != null) {
-            String fileName = ze.getName();
-            File newFile = new File(destination, fileName);
-            if (ze.isDirectory()) {
-                newFile.mkdirs();
-            }
-            else {
-                new File(newFile.getParent()).mkdirs();
-                FileOutputStream fos = new FileOutputStream(newFile);
-                int len;
-                while ((len = zis.read(buffer)) > 0) {
-                    fos.write(buffer, 0, len);
+        try (ZipInputStream zis = new ZipInputStream(in)) {
+            ZipEntry ze = zis.getNextEntry();
+            while (ze != null) {
+                File newFile = new File(destination, ze.getName());
+                if (ze.isDirectory()) { newFile.mkdirs(); }
+                else {
+                    new File(newFile.getParent()).mkdirs();
+                    try (FileOutputStream fos = new FileOutputStream(newFile)) {
+                        int len;
+                        while ((len = zis.read(buffer)) > 0) { fos.write(buffer, 0, len); }
+                    }
                 }
-                fos.close();
+                ze = zis.getNextEntry();
             }
-            ze = zis.getNextEntry();
+            zis.closeEntry();
         }
-        zis.closeEntry();
-        zis.close();
-        in.close();
+        finally {
+            try { in.close(); }
+            catch (IOException ignore) { }
+        }
     }
 }
