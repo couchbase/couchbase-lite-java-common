2a7c3331b780ef3a9bd0b46f6da5f52e953e52a6
LaurenNguyen14 <lauren.nguyen@couchbase.com>
2022-05-24 13:06:51 -0700

    Create C4Collection

--- common/main/java/com/couchbase/lite/internal/core/impl/NativeC4Collection.java
+++ common/main/java/com/couchbase/lite/internal/core/impl/NativeC4Collection.java
@@ -18,37 +18,105 @@ package com.couchbase.lite.internal.core.impl;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.couchbase.lite.internal.core.C4Collection;
 
-@SuppressWarnings("PMD.UnusedPrivateMethod")
-public class NativeC4Collection {
+
+@SuppressWarnings({"PMD.UnusedPrivateMethod", "PMD.TooManyMethods"})
+public class NativeC4Collection implements C4Collection.NativeImpl {
+
+    @Override
+    public long nCreateCollection(long c4Db, @Nullable String scope, @NonNull String collection) {
+        return createCollection(c4Db, scope, collection);
+    }
+
+    @Override
+    public boolean nIsValid(long c4collection) { return isValid(c4collection); }
+
+    @Override
+    public long nGetDatabase(long c4collection) { return getDatabase(c4collection); }
+
+    @Override
+    public long nGetDocumentCount(long c4collection) { return getDocumentCount(c4collection); }
+
+    @Override
+    public long nGetLastSequence(long c4collection) { return getLastSequence(c4collection); }
+
+    @Override
+    public long nGetDoc(long c4Collection, @NonNull String docID, boolean mustExist, long contentLevel) {
+        return getDoc(c4Collection, docID, mustExist, contentLevel);
+    }
+
+    @Override
+    public long nGetDocBySequence(long c4Collection, long seq) { return getDocBySequence(c4Collection, seq); }
+
+    @Override
+    public long nPutDoc(long c4Collection, long request) {
+        return putDoc(c4Collection, request);
+    }
+
+    @Override
+    public long nCreateDoc(long c4Collection, @NonNull String docID, @NonNull byte[] body, int revisionFlags) {
+        return createDoc(c4Collection, docID, body, revisionFlags);
+    }
+
+    @Override
+    public boolean nMoveDoc(long c4Collection, @NonNull String docID, long toCollection, @NonNull String newDocID) {
+        return moveDoc(c4Collection, docID, toCollection, newDocID);
+    }
+
+    @Override
+    public boolean nPurgeDoc(long c4Collection, @NonNull String docID) {
+        return purgeDoc(c4Collection, docID);
+    }
+
+    @Override
+    public boolean nSetDocExpiration(long c4Collection, @NonNull String docID, long timestamp) {
+        return setDocExpiration(c4Collection, docID, timestamp);
+    }
+
+    @Override
+    public long nGetDocExpiration(long c4Collection, @NonNull String docID) {
+        return getDocExpiration(c4Collection, docID);
+    }
+
+    @Override
+    public long nNextDocExpiration(long c4collection) { return nextDocExpiration(c4collection); }
+
+    @Override
+    public long nPurgeExpiredDocs(long c4collection) { return purgeExpiredDocs(c4collection); }
+
+    @Override
+    public boolean nCreateIndex(
+        long c4Collection,
+        String name,
+        String indexSpec,
+        int queryLanguage,
+        int indexType,
+        String language,
+        boolean ignoreDiacritics) {
+        return createIndex(c4Collection, name, indexSpec, queryLanguage, indexType, language, ignoreDiacritics);
+    }
+
+    @Override
+    public boolean nDeleteIndex(long c4Collection, @NonNull String name) { return deleteIndex(c4Collection, name); }
+
+    @Override
+    public long nGetIndexesInfo(long c4Collection) { return getIndexesInfo(c4Collection); }
 
     //-------------------------------------------------------------------------
     // native methods
     //-------------------------------------------------------------------------
 
-    private static native long getDefaultCollection(long c4Db);
-
-    private static native boolean hasCollection(long c4Db, @Nullable String scope, @NonNull String collection);
-
-    private static native long getCollection(long c4Db, @Nullable String scope, @NonNull String collection);
 
     private static native long createCollection(long c4Db, @Nullable String scope, @NonNull String collection);
 
-    private static native boolean deleteCollection(long c4Db, @Nullable String scope, @NonNull String collection);
+    private static native boolean isValid(long c4Collection);
 
-    @NonNull
-    private static native Object getCollectionNames(long c4Db, @NonNull String inScope);
+    private static native long getDatabase(long c4Collection);
 
-    @NonNull
-    private static native Object getScopeNames(long c4Db);
+    private static native long getDocumentCount(long c4Collection);
 
-    private static native boolean isValid(long c4collection);
-
-    private static native long getDatabase(long c4collection);
-
-    private static native long getDocumentCount(long c4collection);
-
-    private static native long getLastSequence(long c4Db);
+    private static native long getLastSequence(long c4Collection);
 
     private static native long getDoc(long c4Collection, @NonNull String docID, boolean mustExist, long contentLevel);
 
@@ -74,9 +142,9 @@ public class NativeC4Collection {
 
     private static native long getDocExpiration(long c4Collection, @NonNull String docID);
 
-    private static native long nextDocExpiration(long c4Db);
+    private static native long nextDocExpiration(long c4Collection);
 
-    private static native long purgeExpiredDocs(long c4Db);
+    private static native long purgeExpiredDocs(long c4Collection);
 
     private static native boolean createIndex(
         long c4Collection,
@@ -84,7 +152,8 @@ public class NativeC4Collection {
         String indexSpec,
         int queryLanguage,
         int indexType,
-        byte[] indexOptions);
+        String language,
+        boolean ignoreDiacritics);
 
     private static native boolean deleteIndex(long c4Collection, @NonNull String name);
 
