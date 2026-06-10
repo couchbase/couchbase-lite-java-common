5435954bb44827f83cd397f4cabe1bd2276dc726
Jim Borden <jim.borden@couchbase.com>
2026-06-10 16:24:21 +0900

    Revert commit that optimizes conflicts for VV
    The remote doc has to win for rev tree

--- common/main/java/com/couchbase/lite/AbstractDatabase.java
+++ common/main/java/com/couchbase/lite/AbstractDatabase.java
@@ -1501,19 +1501,8 @@ abstract class AbstractDatabase extends BaseDatabase
         throws LiteCoreException {
         byte[] mergedBodyBytes = null;
 
-        // Determine the actual winner and loser based on which doc was chosen
-        final String winningRevID;
-        final String losingRevID;
-        if (resolvedDoc == localDoc) {
-            winningRevID = localDoc.getRevisionID();
-            losingRevID = remoteDoc.getRevisionID();
-        } else {
-            winningRevID = remoteDoc.getRevisionID();
-            losingRevID = localDoc.getRevisionID();
-        }
-
-        // Generate merged body only for the merge case (not local-wins or remote-wins)
-        if (resolvedDoc != localDoc && resolvedDoc != remoteDoc) {
+        // Unless the remote revision is being used as-is, we need a new revision:
+        if (resolvedDoc != remoteDoc) {
             if ((resolvedDoc == null) || resolvedDoc.isDeleted()) {
                 mergedFlags |= C4Constants.RevisionFlags.DELETED;
                 try (FLEncoder enc = getSharedFleeceEncoder()) {
@@ -1535,8 +1524,8 @@ abstract class AbstractDatabase extends BaseDatabase
         // Ask LiteCore to do the resolution:
         final C4Document rawDoc = Preconditions.assertNotNull(localDoc.getC4doc(), "raw doc is null");
 
-        // Pass the actual winner and loser revision IDs
-        rawDoc.resolveConflict(winningRevID, losingRevID, mergedBodyBytes, mergedFlags);
+        // The remote branch has to win so that the doc revision history matches the server's.
+        rawDoc.resolveConflict(remoteDoc.getRevisionID(), localDoc.getRevisionID(), mergedBodyBytes, mergedFlags);
         rawDoc.save(0);
 
         Log.d(DOMAIN, "Conflict resolved as doc '%s' rev %s", localDoc.getId(), rawDoc.getRevID());
